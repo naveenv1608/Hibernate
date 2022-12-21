58f8262de5cdf801a92f14611baf1730058fc621
naveen <navee@Naveen_V>
2022-12-21 21:16:00 +0530

    Hibernate First Project

---
+++ HibernateProject/src/main/java/com/nv/test/SelectTest.java
@@ -0,0 +1,43 @@
+package com.nv.test;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import com.nv.entity.Product;
+
+public class SelectTest {
+
+	public static void main(String[] args) {
+		Configuration cfg = new Configuration();
+		cfg.configure("com/nv/config/hibernate.cfg.xml");
+
+		SessionFactory factory = cfg.buildSessionFactory();
+		Session ses = factory.openSession();
+
+		Transaction txn = null;
+
+		try {
+			txn = ses.beginTransaction();
+
+			Product prod = new Product();
+			prod.setPid(4);
+			prod.setPname("Naveen");
+			prod.setPrice(50f);
+			prod.setQty(10f);
+
+			ses.save(prod);
+
+			txn.commit();
+			System.out.println("Record Inserted Successfully");
+
+		} catch (HibernateException he) {
+			he.printStackTrace();
+		}
+		ses.close();
+		factory.close();
+	}
+
+}
